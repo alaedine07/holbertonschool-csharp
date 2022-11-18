17676376441517b2da8f0b850d71184540943709
alaedine07 <alaedineboufaden@gmail.com>
2022-11-18 23:22:09 +0100

    init projects

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0,j = 1;
+            while(i < 9){
+                Console.Write($"{i}{j}");
+                if (i == 8 && j == 9){
+                    Console.WriteLine("");
+                    j++;
+                    i++;
+
+                }
+                else
+                {
+                    Console.Write(", ");
+                }
+                if(j < 9){
+                    j++;
+                }
+                else
+                {
+                    i++;
+                    j = i +1;
+                }
+
+            }
+        }
+    }
+}

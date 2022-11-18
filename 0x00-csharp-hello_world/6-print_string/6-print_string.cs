821bdd993066ad71ef9b00db3a9887ce9f7ba921
alaedine07 <alaedineboufaden@gmail.com>
2022-11-18 20:21:25 +0100

    print String

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.Write("{0}{0}{0}\n", str);
+		Console.WriteLine(str.Substring(0, 9));
+        }
+}
